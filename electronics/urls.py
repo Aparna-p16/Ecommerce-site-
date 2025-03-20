30d8d2a20345dafe8d24c4c88279b33f729adabe
Aparna pawar <aparnapawar162003@gmail.com>
2025-03-20 17:27:58 +0530

    updated

--- electronics/urls.py
+++ electronics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from electronics import views
 
-urlpatterns=[
-    path('product/',views.product_detail,name="product_details")
+urlpatterns = [
+    path('product/',views.product_detail,name="product_details"),
+    path('update/<int:id>',views.update_product ,name='update_product'),
+    path('delete_product/<int:id>',views.delete_product ,name="delete_product"),
 ] 
--- electronics/views.py
+++ electronics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product 
 from .forms import ProductForm
 
@@ -10,10 +10,32 @@ def home(request):
 def product_detail(request):
      prod = Product.objects.all()
      form= ProductForm()
+
      if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-     return render(request,'Electronics/Product_details.html',{'prod':prod,'form':form})
+            form=ProductForm()
+     return render(request,'electronics/Product_details.html',{'prod':prod,'form':form})
 
 
+def update_product(request, id):
+     prod = get_object_or_404 (Product, id=id)            #get the product or return 404
+     
+     if request.method == 'POST':
+        form = ProductForm(request.POST ,instance=prod)    #bind tge form to product instance
+        if form.is_valid():
+            form.save()                                    #save the update products
+            return redirect('product_details')              #redirect to the product list after saving 
+     else:
+        form=ProductForm(instance=prod)                    #populate the form with existing producvt details
+
+     return render(request,'electronics/update_product.html',{'form':form , 'prod':prod})
+
+
+def delete_product(request, id):
+     prod = get_object_or_404 (Product, id=id)             #safe way to get product or return 404 not found
+     prod.delete()
+     return redirect('product_details')                     #redirect back to product details page after deletion
+
+    
